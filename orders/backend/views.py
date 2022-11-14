d548a555f771a65ac228c4ca916f57fd1090f813
alextuchak <alextuchak@gmail.com>
2022-11-15 00:56:46 +0500

    Dockstrings added to views.py

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -1,5 +1,4 @@
 from rest_framework.authentication import TokenAuthentication
-
 from .forms import UploadFileForm
 from django.http import JsonResponse
 from rest_framework.views import APIView
@@ -27,10 +26,16 @@ from django.db import IntegrityError
 
 class RegisterAccount(APIView):
     """
-        Класс для регистрации пользователей
+    Класс регистрации аккаунта. Доступен HTTP method post. За сериализацию данных отвечает класс UserSerializer.
     """
 
     def post(self, request, *args, **kwargs):
+        """
+        HTTP method post. В теле json-запроса должны присутствовать поля first_name, last_name, email, password, type.
+        Пароль проверяется на сложность методом validate_password. За валидацию всех данных reauest.data отвечает
+        UserSerializer. При успешной валидации создается объект класса User, а на указанную почту отправляется email
+        с помощью celery task new_user_registered_task
+        """
         if {'first_name', 'last_name', 'email', 'password', 'type'}.issubset(request.data):
             errors = {}
             try:
@@ -42,10 +47,8 @@ class RegisterAccount(APIView):
                     error_array.append(item)
                 return JsonResponse({'Status': False, 'Errors': {'password': error_array}}, status=403)
             else:
-                # проверяем данные для уникальности имени пользователя
                 user_serializer = UserSerializer(data=request.data)
                 if user_serializer.is_valid():
-                    # сохраняем пользователя
                     user = user_serializer.save()
                     user.set_password(request.data['password'])
                     user.save()
@@ -59,13 +62,15 @@ class RegisterAccount(APIView):
 
 class ConfirmAccount(APIView):
     """
-        Класс для подтверждения и активации аккаунта
+    Класс для подтверждения и активации аккаунта. Доступен http method post.
     """
 
-    # Регистрация методом POST
     def post(self, request, *args, **kwargs):
-
-        # проверяем обязательные аргументы
+        """
+        HTTP method post. В теле json-запроса должны присутствовать поля email, token. Токен направляется пользователю
+        на email после регистрации. При соответствии пары email, token поле is_active в модели User устанавливается в
+        значение True, a токен удаляется
+        """
         if {'email', 'token'}.issubset(request.data):
 
             token = ConfirmEmailToken.objects.filter(user__email=request.data['email'],
@@ -83,13 +88,17 @@ class ConfirmAccount(APIView):
 
 class AccountDetails(APIView):
     """
-        Класс для работы с информацией об аккаунте
+    Класс для просмотра информации об аккаунте и изменения данных пользователя. Доступен http method get, post. За
+    аутентификацию отвечает класс TokenAuthentication
     """
 
     authentication_classes = (TokenAuthentication,)
 
-    # получить данные
     def get(self, request, *args, **kwargs):
+        """
+        HTTP method get. Метод для получения информации об аккаунте. После проверки методом is_authenticated
+        возвращается экземлпяр класса User, за сериализацию данных отвечает класс AccountDetailSerializer
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
 
@@ -97,18 +106,20 @@ class AccountDetails(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
+        """
+        HTTP method get. Метод для изменения информации об аккаунте. После проверки методом is_authenticated проверяется
+        наличие ключа 'password' в request.data. При обнаружении ключа пароль вроверяется методом validate_password на
+        сложность и затем записывается в модель User. За валидацию и изменение других полей модели User овечает класс
+        UserSerialier
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
-        # проверяем обязательные аргументы
-
         if 'password' in request.data:
             errors = {}
-            # проверяем пароль на сложность
             try:
                 validate_password(request.data['password'])
             except Exception as password_error:
                 error_array = []
-                # noinspection PyTypeChecker
                 for item in password_error:
                     error_array.append(item)
                 return JsonResponse({'Status': False, 'Errors': {'password': error_array}}, status=400)
@@ -117,7 +128,6 @@ class AccountDetails(APIView):
                 request.user.save()
         else:
             return JsonResponse({'Status': False, 'Error': 'Не указаны все необходимые аргументы'}, status=403)
-        # проверяем остальные данные
         user_serializer = UserSerializer(request.user, data=request.data, partial=True)
         if user_serializer.is_valid():
             user_serializer.save()
@@ -128,12 +138,15 @@ class AccountDetails(APIView):
 
 class LoginAccount(APIView):
     """
-        Класс для авторизации пользователей
+    Класс для авторизации пользователя. Доступен http method post
     """
 
-    # Авторизация методом POST
     def post(self, request, *args, **kwargs):
-
+        """
+        HTTP method post. Метод для авторизации пользователя. В request.data проверяется наличие ключей 'email' и
+        'password'. Авторизация просходит методом authentificate который возвращает объект класса User при совпадении
+        полей email и password. Если поле is_active=True то для объекта класса создается TokenAuthentication
+        """
         if {'email', 'password'}.issubset(request.data):
             user = authenticate(request, username=request.data['email'], password=request.data['password'])
             if user is not None:
@@ -148,11 +161,17 @@ class LoginAccount(APIView):
 
 class ShopUpload(APIView):
     """
-        Класс для импорта товорав в магазин
+    Класс для обновления прайса магазина с помощью .yaml файла отправленного через http запрос. Доступен http method
+    post. За аутентификацию отвечает класс TokenAuthentication
     """
     authentication_classes = (TokenAuthentication,)
 
     def post(self, request):
+        """
+        HTTP method post. Метод для загрузки прайса товаров из .yaml файла. После проверки методом is_authenticated
+        проверяется тип пользователя. Файл из http запроса загружается в file_form модели ShopFile. После проверки
+        валидности формы вызывается celery task handle_uploaded_file_task отвечающий за обновление прайса товаров
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
         if request.user.type != 'seller':
@@ -170,7 +189,8 @@ class ShopUpload(APIView):
 
 class CategoryViewSet(ModelViewSet):
     """
-        Класс для просмотра категорий товаров
+    Класс для получения списка категорий товаров. Доступен http method get. За сериализацию данных отвечает класс
+    CategorySerializer. Фильтрация доступна по полю name
     """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -181,7 +201,8 @@ class CategoryViewSet(ModelViewSet):
 
 class ShopViewSet(ModelViewSet):
     """
-        Класс для просмотра магазинов
+    Класс для получения списка магазинов. Доступен http method get. За сериализацию данных отвечает класс
+    ShopSerializer. Фильтрация доступна по полю name, is_work
     """
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
@@ -192,7 +213,8 @@ class ShopViewSet(ModelViewSet):
 
 class ProductViewSet(ModelViewSet):
     """
-        Класс для просмотра товара
+    Класс для получения списка товаров. Доступен http method get. За сериализацию данных отвечает класс
+    ProductSerializer. Фильтрация доступна по полю name, model. Поиск доступен по полю name, model
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -204,7 +226,8 @@ class ProductViewSet(ModelViewSet):
 
 class ShopProductViewSet(ModelViewSet):
     """
-        Класс для просмотра товара в конкретном магазине
+    Класс для получения списка товаров в конкретном магазине. Доступен http method get. За сериализацию данных отвечает
+    класс ShopProductSerializer. Поиск доступен по полям product__model, product__name (Поля model и name модели Product)
     """
     queryset = ShopProduct.objects.all()
     serializer_class = ShopProductSerializer
@@ -215,7 +238,8 @@ class ShopProductViewSet(ModelViewSet):
 
 class ProductInfViewSet(ModelViewSet):
     """
-        Класс для просмотра информации о товаре
+    Класс для получения списка информации о товаре. Доступен http method get. За сериализацию данных отвечает класс
+    ProduceInfSerializer. Поиск доступен по полям product_id__model, product_id__name (Поля model и name модели Product)
     """
     queryset = ProductInf.objects.all()
     serializer_class = ProductInfSerializer
@@ -226,11 +250,17 @@ class ProductInfViewSet(ModelViewSet):
 
 class UserContact(APIView):
     """
-        Класс для работы с контактной информацией пользователя
+    Класс для работы с контактной информацией пользователя. Доступен http method get, post, put, delete. За
+    аутентификацию отвечает класс TokenAuthentication
     """
     authentication_classes = (TokenAuthentication,)
 
     def get(self, request, *args, **kwargs):
+        """
+        HTTP method get. Метод для получения контактной информации о пользователе. После проверки методом
+        is_authenticated возвращается экземлпяр класса Contact относящийся к пользователю выполневшему запрос. За
+        сериализацию данных отвечает класс ContactSerializer
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
         contact = Contact.objects.filter(user_id=request.user.id)
@@ -238,6 +268,12 @@ class UserContact(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
+        """
+        HTTP method post. Метод для создания контактной информации о пользователе. После проверки методом
+        is_authenticated проверяется наличие ключей 'country', 'region', 'zip', 'city', 'street', 'house', 'phone' в
+        request.data. После валидации данных методом is_valid данные сохраняются в БД. За сериализацию данных отвечает
+        класс ContactSerializer
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
         if {'country', 'region', 'zip', 'city', 'street', 'house', 'phone'}.issubset(request.data):
@@ -251,6 +287,12 @@ class UserContact(APIView):
         return JsonResponse({'Status': False, 'Error': 'Не указаны все необходимые аргументы'}, status=403)
 
     def delete(self, request, *args, **kwargs):
+        """
+        HTTP method delete. Метод для удаления контактной информации о пользователе. После проверки методом
+        is_authenticated проверяется наличие ключа 'items' в request.data. При нахождении записи в БД с запрашиваемым id
+        и принадлежащей пользователю выполневшему запрос, запись удаляется. За сериализацию данных отвечает класс
+        ContactSerializer
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
 
@@ -263,13 +305,18 @@ class UserContact(APIView):
                 if contact_id.isdigit():
                     query = query | Q(user_id=request.user.id, id=contact_id)
                     objects_deleted = True
-
             if objects_deleted:
                 deleted_count = Contact.objects.filter(query).delete()[0]
                 return JsonResponse({'Status': True, 'Удалено объектов': deleted_count})
         return JsonResponse({'Status': False, 'Error': 'Не указаны все необходимые аргументы'}, status=403)
 
     def put(self, request, *args, **kwargs):
+        """
+        HTTP method put. Метод для изменения контактной информации о пользователе. После проверки методом
+        is_authenticated проверяется наличие ключа 'id' в request.data. При нахождении записи в БД с запрашиваемым id
+        и принадлежащей пользователю выполневшему запрос, запись обновляется. За сериализацию данных отвечает класс
+        ContactSerializer
+        """
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
 
@@ -289,7 +336,8 @@ class UserContact(APIView):
 
 class BasketViewSet(ModelViewSet):
     """
-    Класс для выполнения операций с корзиной товаров
+    Класс для работы с корзиной товаров пользователя. Доступен http method get, post, put, delete. За
+    аутентификацию отвечает класс TokenAuthentication
     """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
@@ -298,6 +346,11 @@ class BasketViewSet(ModelViewSet):
 
     #
     def get_queryset(self):
+        """
+        HTTP method get. Метод для получения информации о товаре в корзине пользователя. После проверки методом
+        is_authenticated возвращается экземлпяр класса Order с статусом 'basket' относящийся к пользователю выполневшему
+        запрос. За сериализацию данных отвечает класс OrderSerializer.
+        """
         queryset = Order.objects.filter(user_id=self.request.user.id, status='basket'). \
             prefetch_related('ordered_items').annotate(
             total_sum=Sum(F('ordered_items__quantity') * F('ordered_items__product_info__price'))).distinct()
@@ -305,6 +358,11 @@ class BasketViewSet(ModelViewSet):
 
     #
     def create(self, request, *args, **kwargs):
+        """
+        HTTP method post. Метод для создания корзины товаров пользователя. После проверки методом
+        is_authenticated создается объект класса Order. Товар и его количество сохраняется в объектах класса OrderItem.
+        За сериализацию данных отвечает класс OrderItemSerializer
+        """
         if self.request.data:
             try:
                 objects_created = 0
@@ -327,9 +385,15 @@ class BasketViewSet(ModelViewSet):
     #
     @action(methods=['delete'], detail=False)
     def delete(self, request, *args, **kwargs):
+        """
+        HTTP method delete. Метод для удаления контактной информации о пользователе. После проверки методом
+        is_authenticated проверяется наличие ключа 'items' в request.data. При нахождении записи в БД принадлежащей
+        пользователю выполневшему запрос c запрашиваемым id корзины и статусом 'basket', запись удаляется. За
+        сериализацию данных отвечает класс OrderItemSerializer
+        """
         try:
             items_to_del = str(self.request.data['items']).split(',')
-        except KeyError :
+        except KeyError:
             return JsonResponse({'Status': False, 'Error': 'Не указаны все необходимые аргументы'}, status=403)
         if items_to_del:
             basket, _ = Order.objects.get_or_create(user_id=self.request.user.id, status='basket')
@@ -351,6 +415,12 @@ class BasketViewSet(ModelViewSet):
     #
     @action(methods=['put'], detail=False)
     def put(self, request, *args, **kwargs):
+        """
+        HTTP method put. Метод для изменения cсодержимого корзины пользователя. После проверки методом
+        is_authenticated происходит валидация данных методом is_valid. При нахождении записи в БД с запрашиваемым
+        товаром и принадлежащей пользователю выполневшему запрос и статусом 'basket', обновляется количество товара. За
+        сериализацию данных отвечает класс OrderItemSerializer
+        """
         if self.request.data:
             try:
                 objects_updated = 0
@@ -373,7 +443,8 @@ class BasketViewSet(ModelViewSet):
 
 class OrderViewSet(ModelViewSet):
     """
-    Класс для выполнения операций с заказами
+    Класс для работы с заказами пользователя. Доступен http method get, post. За аутентификацию отвечает класс
+    TokenAuthentication
     """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
@@ -381,12 +452,23 @@ class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """
+        HTTP method get. Метод для получения информации о заказах пользователя. После проверки методом
+        is_authenticated возвращается экземлпяр класса Order с любым статусом, но не 'basket' относящийся к пользователю
+        выполневшему запрос. За сериализацию данных отвечает класс OrderSerializer.
+        """
         queryset = Order.objects.filter(user_id=self.request.user.id).exclude(status='basket').prefetch_related(
             'ordered_items').annotate(
             total_sum=Sum(F('ordered_items__quantity') * F('ordered_items__product_info__price'))).distinct()
         return queryset
 
     def create(self, request, *args, **kwargs):
+        """
+        HTTP method post. Метод для создания корзины товаров пользователя. После проверки методом
+        is_authenticated статус объекта класса Order, относящего к пользователю выполневшему запрос, c id указанным в
+        запросе обновляется на 'new'. Далее при наличии контактной информации пользователя вызывается celery task
+        new_order_task и new_order_for_seller_task для оповещения продавца и покупателя о создании нового заказа.
+        """
         if {'id'}.issubset(self.request.data):
             if self.request.data['id'].isdigit():
                 try:
@@ -415,8 +497,8 @@ class OrderViewSet(ModelViewSet):
 
 class SellerOrderViewSet(ModelViewSet):
     """
-       Класс для продавцов для выполнения операций с заказами
-
+    Класс для продавцов для работы с заказами пользователей. Доступен http method get, put. За аутентификацию отвечает
+    класс TokenAuthentication
     """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
@@ -424,6 +506,11 @@ class SellerOrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """
+        HTTP method get. Метод для получения информации о заказах пользователя. После проверки методом
+        is_authenticated возвращается все экземлпяры класса Order с любым статусом, но не 'basket' относящиеся к
+        продавцу выполневшему запрос. За сериализацию данных отвечает класс OrderSerializer.
+        """
         if self.request.user.type != 'seller':
             return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
         queryset = Order.objects.exclude(status='basket').prefetch_related(
@@ -433,6 +520,13 @@ class SellerOrderViewSet(ModelViewSet):
 
     @action(methods=['put'], detail=False)
     def put(self, request, *args, **kwargs):
+        """
+        HTTP method put. Метод для изменения статуса заказа продавцом. После проверки методом
+        is_authenticated происходит проверка типа пользователя. При нахождении записи в БД с запрашиваемым
+        id обновляется статус заказа. После обновления статуса заказа вызывается celery task order_status_change_task
+        Для оповещения продавца и покупателя об изменении статуса заказа.
+        За сериализацию данных отвечает класс OrderItemSerializer
+        """
         if request.user.type != 'seller':
             return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
         if 'id' and 'status' in self.request.data:
